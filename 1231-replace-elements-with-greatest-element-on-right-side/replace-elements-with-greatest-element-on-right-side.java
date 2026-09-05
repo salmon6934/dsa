10877989c2ef650db129d19add30f984889f8fe7
Harsh Nayan <nayan12072004@gmail.com>
2026-09-05 22:01:54 +0530

    Time: 3 ms (68.98%) | Memory: 49.2 MB (34.41%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+        int max=-1;
+        for(int i=n-1;i>=0;i--){
+            int m=arr[i];
+            arr[i]=max;
+            max=Math.max(max,m);
+        }
+        return arr;
+    }
+}
